89dae66fff4627cdc193121b586a4f8f1173e33e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2014-10-19 09:48:00 +0900

    fix test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "registration" do
-    mail = UserMailer.registration
+    mail = UserMailer.registration(users(:one))
     assert_equal "Registration", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
